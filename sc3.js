a32945e1802510e1af4c5b3395c7fb600fd5f231
mathnasiumscripts <89877053+mathnasiumscripts@users.noreply.github.com>
2021-11-15 15:04:46 -0800

    Update sc3.js

--- sc3.js
+++ sc3.js
@@ -7,7 +7,7 @@
 // ==/UserScript==
 
 
-var wbClient = "/client/js/app-0b51b5f4f08e0796c53a.js";
+var wbClient = "/client/js/app-42615ea6e8068030a191.js";
 
 var modInfo = {
     "table1":{col:"#00ff00",inl:"KC",tnum:"T1",tutoring:true,super:true,incenter:true},
